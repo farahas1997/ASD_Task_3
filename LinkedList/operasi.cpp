8b5f0baba07ad35a8695f2c50ce63cedafba1f8a
Muhammad Fadhlan Supriadi <farahas1997@gmail.com>
2016-09-23 03:05:55 +0700

    Final ASD Task 3 / Kelompok 10

--- LinkedList/operasi.cpp
+++ LinkedList/operasi.cpp
@@ -12,14 +12,43 @@ address sentinelSearch(List *L, infotype x){
     *      mengembalikan NULL jika tidak ditemukan
     */
     //-------------your code here-------------
-    // NIM : Dedep
+    // NIM : 1301154454 / Dedep
+    address P;
+    P = L->first;
+    if (L->first != NULL)
+    {
+        if (L->first->next != NULL && L->first->info.id != x.id)
+        {
+            infotype z = info(P);
+            insertLast(L,alokasi(x));
+            while (z.id!= x.id)
+            {
+                P = P->next;
+                z = P->info;
+            }
+            address Q;
+            deleteLast(L,Q);
+            if ( P->next != NULL)
+                return P;
+            else
+                return NULL;
+        }
+        else
+        {
+            return L->first;
+        }
+    }
+    else
+    {
 
+        return NULL;
+    }
 
     //----------------------------------------
 
 }
 
-void insertionSort(List &L){
+void insertionSort(List *L){
     /**
     * IS : List mungkin kosong
     * PR : mengurutkan elemen di dalam List L menggunakan algoritma insertion sort
@@ -27,36 +56,44 @@ void insertionSort(List &L){
     */
 
     //-------------your code here-------------
-    // NIM : 1301140165
+    // NIM : 1301140165 / Fajar
     int temp,ketemu,z;
     address p,q;
     temp=NULL;
-        if((L.first==NULL)||(L.first)->next==NULL){
-            return;
-        }
-        p=next(L.first);
-        while(p!=NULL){
-            z=p->info.id;
-            ketemu=0;
-            q=L.first;
-            while(q!=p){
-                if(q->info.id > p->info.id&&ketemu==0){
-                    z=q->info.id;
-                    q->info.id = p->info.id;
-                    ketemu = 1;
-                    q=q->next;
-                }else{
-                    if(ketemu==1){
-                        temp=z;
-                        z=q->info.id;
-                        q->info.id=temp;
-                    }
-                    q=q->next;
-                }
+    if((L->first==NULL)||(L->first)->next==NULL)
+    {
+        return;
+    }
+    p=next(L->first);
+    while(p!=NULL)
+    {
+        z=p->info.id;
+        ketemu=0;
+        q=L->first;
+        while(q!=p)
+        {
+            if(q->info.id > p->info.id &&ketemu==0)
+            {
+                z=q->info.id;
+                q->info.id = p->info.id;
+                ketemu = 1;
+                q=q->next;
             }
-            q->info.id=z;
-            p=p->next;
+            else
+            {
+                if(ketemu==1)
+                {
+                    temp=z;
+                    z=q->info.id;
+                    q->info.id=temp;
+                }
+                q=q->next;
+            }
         }
+        q->info.id=z;
+        p=p->next;
+    }
+
     //----------------------------------------
 }
 
@@ -69,8 +106,10 @@ void deletebyID(List &L, infotype x) {
 
     address Prec, P;
     //-------------your code here-------------
-    // NIM :
-
-
+    // NIM : 1301154202 / Fadhlan
+    Prec = findElm(L,x);
+    deleteAfter(&L,P);
+    cout<<"Masukkan ID Karyawan yang akan dihapus: "<<x.id<<endl;
+    dealokasi(P);
     //----------------------------------------
 }
